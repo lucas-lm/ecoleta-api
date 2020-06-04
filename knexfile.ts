44509dd2d6e1085b00c3099fe279962c8edaa8c7
Lucas Miranda <lucas-m@outlook.com>
2020-06-04 16:05:34 -0300

    added seed config

--- knexfile.ts
+++ knexfile.ts
@@ -6,4 +6,7 @@ module.exports = {
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
   },
+  seeds: {
+    directory: path.resolve(__dirname, 'src', 'database', 'seeds'),
+  },
 }
