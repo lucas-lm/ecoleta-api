114c924d69016507160c4fd95a5fa9bed9c92e68
Lucas Miranda <lucas-m@outlook.com>
2020-06-04 16:05:14 -0300

    added static assets serve

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import path from 'path'
 import routes from './routes'
 
 const app = express()
 
+app.use('/static', express.static(path.resolve(__dirname, 'assets')))
 app.use(express.json())
 app.use(routes)
 
