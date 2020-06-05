877f697ad31796882e0abbb5bfe0403fa6aa69df
Lucas Miranda <lucas-m@outlook.com>
2020-06-04 23:17:20 -0300

    feat: added cors middleware

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import path from 'path'
 import routes from './routes'
 
 const app = express()
 
+app.use(cors())
 app.use('/static', express.static(path.resolve(__dirname, 'assets')))
 app.use(express.json())
 app.use(routes)
