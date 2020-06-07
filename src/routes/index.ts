cf3af4fa67272cafab0cc4ab0636ab7870a8c32d
Lucas Miranda <lucas-m@outlook.com>
2020-06-06 23:29:10 -0300

    feat: added validation middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,31 @@
 import express from 'express'
+import upload from '../middlewares/upload'
 import pointController from '../controllers/PointController'
 import itemController from '../controllers/ItemController'
+import { celebrate, Joi } from 'celebrate'
 
 const routes = express.Router()
 
-routes.post('/points', pointController.create)
+routes.post(
+  '/points',
+  upload.single('photo'),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    { abortEarly: false }
+  ),
+  pointController.create
+)
 routes.get('/points/:pointId', pointController.show)
 routes.get('/points', pointController.index)
 routes.get('/items', itemController.index)
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import path from 'path'
 import routes from './routes'
+import { errors } from 'celebrate'
 
 const app = express()
 
@@ -9,6 +10,7 @@ app.use(cors())
 app.use('/static', express.static(path.resolve(__dirname, 'assets')))
 app.use(express.json())
 app.use(routes)
+app.use(errors())
 
 const { PORT, NODE_ENV } = process.env
 app.listen(PORT, () => {
